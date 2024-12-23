8d4e3e42f5705897e784bcc2654054cdd6012b78
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-12-23 17:22:45 -0500

    Change header color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,10 @@ import { KanbanIcon } from './icons/KanbanIcon'
 
 export const Header = () => {
   return (
-    <header className=' flex gap-2 h-fit py-2 items-center fixed top-0 bg-[#24242470] backdrop-blur-lg z-20 w-full'>
+    <header
+      className=' flex gap-2 h-fit py-2 items-center fixed top-0 left-0 right-0 bg-[#1a1a1a] backdrop-blur-lg z-20 w-full
+     border-b border-zinc-700 px-[10%]'
+    >
       <KanbanIcon />
       <h1 className=' text-2xl font-bold'>KANBAN APP</h1>
     </header>
