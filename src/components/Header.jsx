33a8d11f83f2cd90f81bad63096da2e0681ca375
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-12-29 14:35:36 -0500

    Add link to home in the logo page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom'
 import { KanbanIcon } from './icons/KanbanIcon'
 
 export const Header = () => {
   return (
     <header
-      className=' flex gap-2 h-fit py-2 items-center fixed top-0 left-0 right-0 bg-[#1a1a1a] backdrop-blur-lg z-20
+      className='h-fit py-2 fixed top-0 left-0 right-0 bg-[#1a1a1a] backdrop-blur-lg z-20
      border-b border-zinc-700 px-[10%]'
     >
-      <KanbanIcon />
-      <h1 className=' text-2xl font-bold'>KANBAN APP</h1>
+      <Link to={'/'} className='flex gap-2 items-center'>
+        <KanbanIcon />
+        <h1 className=' text-2xl font-bold'>KANBAN APP</h1>
+      </Link>
     </header>
   )
 }
