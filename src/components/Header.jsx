371ae48c241a408e72db806f5b56ece7d9e5d37b
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-12-29 10:27:34 -0500

    change width header for full screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { KanbanIcon } from './icons/KanbanIcon'
 export const Header = () => {
   return (
     <header
-      className=' flex gap-2 h-fit py-2 items-center fixed top-0 left-0 right-0 bg-[#1a1a1a] backdrop-blur-lg z-20 w-full
+      className=' flex gap-2 h-fit py-2 items-center fixed top-0 left-0 right-0 bg-[#1a1a1a] backdrop-blur-lg z-20
      border-b border-zinc-700 px-[10%]'
     >
       <KanbanIcon />
