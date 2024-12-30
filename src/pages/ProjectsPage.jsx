9a22f22e27a1c67bf9b448080b23734f811bfff6
Kristian Cifuentes <kdcv2007@hotmail.com>
2024-12-30 17:16:55 -0500

    Fix title position for projects

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -5,10 +5,10 @@ import { ProjectsContainer } from '../components/ProjectsContainer'
 export const ProjectsPage = () => {
   return (
     <>
-      <CreateProject />
       <h2 className='text-xl font-bold text-[var(--principal-color)] mb-4'>
         PROYECTOS
       </h2>
+      <CreateProject />
       <ProjectsContainer />
     </>
   )
