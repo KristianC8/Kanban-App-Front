6ac6b7cf9fbd4349e63d44bdfec378dbdf224bd2
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-01-02 17:15:57 -0500

    Add availability checker component to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,18 @@
 import { Link } from 'react-router-dom'
 import { KanbanIcon } from './icons/KanbanIcon'
+import { IsAvailable } from './isAvailable'
 
 export const Header = () => {
   return (
     <header
       className='h-fit py-2 fixed top-0 left-0 right-0 bg-[#1a1a1a] backdrop-blur-lg z-20
-     border-b border-zinc-700 px-[10%]'
+     border-b border-zinc-700 px-[10%] flex justify-between'
     >
       <Link to={'/'} className='flex gap-2 items-center w-fit'>
         <KanbanIcon />
         <h1 className=' text-2xl font-bold'>KANBAN APP</h1>
       </Link>
+      <IsAvailable />
     </header>
   )
 }
