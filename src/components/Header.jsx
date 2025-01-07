323426a531abf5d178406787d64e5923cd4e3f7f
Kristian Cifuentes <kdcv2007@hotmail.com>
2025-01-07 17:23:07 -0500

    Fiz padding header for mobile screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <header
       className='h-fit py-2 fixed top-0 left-0 right-0 bg-[#1a1a1a] backdrop-blur-lg z-20
-     border-b border-zinc-700 px-[10%] flex justify-between'
+     border-b border-zinc-700 px-2 sm:px-[10%] flex justify-between'
     >
       <Link to={'/'} className='flex gap-2 items-center w-fit'>
         <KanbanIcon />
